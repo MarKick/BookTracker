b8565c9aa701a2e03a8f9761be04a624d52f1e5b
Marc <kick.public@tutanota.com>
2023-11-18 11:40:08 +0100

    lab1: Fixed button styling

--- bookFront/src/Components/AllTabs/BookInfo.js
+++ bookFront/src/Components/AllTabs/BookInfo.js
@@ -1,4 +1,5 @@
 import React, {useState}  from "react";
+import "./bookInfo.css";
 
 const BookInfo = ({props}) => {
     const [dd, setDD] = useState(false); 
@@ -10,17 +11,20 @@ const BookInfo = ({props}) => {
 
     return (
         <div
-              style={{
-                width: "30em",
-                backgroundColor: "#a9a9a9",
-                padding: 2,
-                borderRadius: 10,
-                marginBlock: 10,
-              }}
+        // style={{
+        //   width: "30em",
+        //   backgroundColor: "#a9a9a9",
+        //   padding: 2,
+        //   borderRadius: 10,
+        //   marginBlock: 10,
+        // }}
             >
               {/* TODO: Make these buttons to seperate sites */}
-              <button style={{ fontSize: 15, color: 'black'}} onClick={handleClick}> {props.title} ~ {props.author}</button> 
+              <div>
+                <button className="bookInfoButton" onClick={handleClick}> {props.title} ~ {props.author}</button> 
+              </div>
               
+              {/* className={styles.bookInfoButton} */}
               <div>{dd ? <p>text info</p>: null}</div>
             </div>
     );
